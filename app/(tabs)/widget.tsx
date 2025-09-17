a65887bfe19a656756f1903033ef18f411843e6c
Ryan Turnbull <ryan@lorikeetcx.ai>
2025-09-17 17:17:38 +1000

    fix: header widget

--- app/(tabs)/widget.tsx
+++ app/(tabs)/widget.tsx
@@ -1,48 +1,43 @@
-import { useRef } from "react";
-import { View } from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { WebView } from 'react-native-webview';
+import { LorikeetClient, WidgetScreen } from '@lorikeetai/react-native-widget';
+import { Text, View } from 'react-native';
+import WebView from 'react-native-webview';
 
-const widgetUrl = 'https://chat.lorikeetcx.ai/widget?public_key=wk_a01eebcb-668b-4414-aadc-21fe7102c1e7&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NUb2tlbiI6IjUwMTdlZDg2YzU1MTRkZGI3M2FhZWUyYjk2NjZhOThmYTdjZjY3OTU2OTI2MGZhNmZjOTc2MDViYzY5NDNlYTA2YTQ4Yzk3YWQ1OGRiN2UzZmFhNmExNzU3M2RiZGMzOWEzMDQ4MmFhYTQwYTE3MThiYzJhYzYwZWFlMWM0Zjk3IiwiaWF0IjoxNzU3OTg2ODA0LCJleHAiOjE3NTg1OTE2MDQsImF1ZCI6IjM2NjE3ZWY4LTcxNzMtNGI1Zi04MTVkLWJiZGJjNDYyZTZmOSJ9.YN6lUkLY4L8Ouq6R0WmU1ZxHA_jYh_cfGXmahRL8WGM&customisations=eyJtb2JpbGUiOnsiZGVmYXVsdF9vcGVuIjpmYWxzZX0sImRlc2t0b3AiOnsiZGVmYXVsdF9vcGVuIjp0cnVlfX0%3D';
+const publicKey = 'wk_a01eebcb-668b-4414-aadc-21fe7102c1e7';
+// Testing only
+const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NUb2tlbiI6IjhhNjllM2YzMDNmYjc0NGQwNzMzNWMwOWI1NjQ1MmE2ZWViNTc3ZmZmMTEyZDI2MzAxZTRhOTE1ZTU4YmMwZDY5M2UxMzk4OWM5YWQ4MDMyNmQ1MGNiZWUyMGIwMTg1MjNlYzc1NmI2YjNhZWJkZDdmYjg4Y2NhMzg1MGVlZWI0IiwiaWF0IjoxNzU4MDgzNDU3LCJleHAiOjE3NTg2ODgyNTcsImF1ZCI6IjM2NjE3ZWY4LTcxNzMtNGI1Zi04MTVkLWJiZGJjNDYyZTZmOSJ9.-t_FxQKbFKY-vDg5dAko274AyZGPByxfvQOrlLTN1ZU'
 
-export default function WidgetScreen() {
-    const webViewRef = useRef<WebView>(null);
-
+export const WidgetUsingComponent = () => {
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }} edges={['top', 'left', 'right']}>
-            <View style={{ 
-                flex: 1, 
-            }}>
-                <View style={{ 
-                    flex: 1,
-                    borderRadius: 8,
-                    overflow: 'hidden',
-                    shadowColor: '#000',
-                    shadowOffset: { width: 0, height: 2 },
-                    shadowOpacity: 0.1,
-                    shadowRadius: 4,
-                    elevation: 3
-                }}>
-                    <WebView 
-                        style={{ flex: 1 }} 
-                        source={{ uri: widgetUrl }} 
-                        onMessage={(event) => {
-                            console.log('event', event);
-                        }}
-                        onNavigationStateChange={(navState) => {
-                            console.log('navState', navState);
-                            if (navState.url.includes('chat.lorikeetcx.ai/tickets')) {
-                                return true;
-                            }
-                            return false; // Prevents navigation
-                        }}
-                        allowsInlineMediaPlayback={true}
-                        mediaPlaybackRequiresUserAction={false}
-                        startInLoadingState
-                        scalesPageToFit={true}
-                    />
+        <WidgetScreen 
+            publicKey={publicKey} 
+            authToken={authToken} 
+            renderHeader={client =>(
+                <View style={{ width: '100%', padding: 16, borderBottomWidth: 1, borderBottomColor: 'gray' }}>
+                    <Text>Custom Header, team cool</Text>
                 </View>
-            </View>
-        </SafeAreaView>
+            )}
+            customisations={{
+                title: 'Local development'
+            }} 
+        />
     );
 }
+
+const lorikeetClient = new LorikeetClient({
+    public_key: publicKey,
+    customisations: {
+        title: 'Local development'
+    }
+});
+
+const url = lorikeetClient.getWebviewUrl({
+    token: authToken,
+});
+
+export const WidgetUsingClient = () => {
+    return (
+        <WebView source={{ uri: url }} />
+    );
+}
+
+export default WidgetUsingComponent
